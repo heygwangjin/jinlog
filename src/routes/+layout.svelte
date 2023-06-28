041b4bf84a082e013c42eef4b4db36fd7430c417
heygwangjin <heygwangjin@gmail.com>
2023-06-28 22:44:48 +0900

    add

--- src/routes/+layout.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import { base } from '$app/paths';
-	import '../app.css';
-</script>
-
-<nav>
-	<a href="{base}/">Home</a>
-</nav>
-<slot />
--- src/routes/+page.svelte
+++
@@ -1,3 +0,0 @@
-<h1 class="text-3xl font-bold text-orange-500">
-	Welcome to Sveltekit + TailwindCSS + Supabase Template
-</h1>
